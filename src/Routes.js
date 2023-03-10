aa085ad2846bbcdc76fd227bf1fcca4f890e4b27
2damt <2damt@LP9P3A2C.medusa.gobiernodecanarias.net>
2023-03-10 13:27:51 +0000

    Login page

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Login from './pages/Login/Login'
 
-function AppRoutes() {
+function AppRoutes () {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -14,7 +14,7 @@ export default function About () {
         <p className='about-text'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac tortor id mi pharetra interdum. Quisque vel augue eget mauris blandit egestas. Mauris ut tellus quis elit iaculis vestibulum eget sed tellus. Nunc efficitur lobortis nisl, in rhoncus leo molestie sed. Vestibulum interdum auctor nulla, at consequat felis lobortis quis. Integer pharetra neque non velit tincidunt maximus. Sed interdum dui sit amet quam suscipit, nec faucibus nulla eleifend. Nam in ultricies sapien. Donec pellentesque felis vitae sapien rutrum, ac mollis leo vehicula. Aliquam finibus enim vitae lorem lacinia, vel tristique mauris tristique. Nam in interdum velit. Ut porttitor dolor ut ipsum vehicula, sit amet scelerisque dolor laoreet.
         </p>
-        <button className='about-button'><a href='https://sdgs.un.org/es/goals' target='_blank' rel='noopener noreferrer'>Learn More</a></button>
+        <button className='about-button'><a href='https://sdgs.un.org/es/goals' className='about-link' target='_blank' rel='noopener noreferrer'>Learn More</a></button>
       </div>
       <Footer />
     </div>
