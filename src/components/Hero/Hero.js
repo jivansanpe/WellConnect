77e4ce65b9fc2ec6a9f9bde7da57a612fd4d6dc7
Jón Ólafur <jolafur4@gmail.com>
2023-03-13 09:48:16 +0000

    search bar and dropdown

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './Hero.module.css'
 
-export default function Hero () {
+export default function Hero ({ title, body }) {
   return (
     <section className={styles.Hero}>
-      <h1 className={styles['hero--header']}>Online Experiences</h1>
-      <p className={styles['hero--text']}>Join unique interactive activities led by
-        one-of-a-kind hosts—all without leaving home.</p>
+      <h1 className={styles['hero--header']}>{title}</h1>
+      <p className={styles['hero--text']}>{body}</p>
     </section>
   )
+}
+
+Hero.propTypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired
 }
