9b2e145b9af06d68f9f07491b797e8945b950851
Tobias Aagaard <Tobias909@live.dk>
2023-03-14 10:53:32 +0100

    404 page for not found pages

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Login from './pages/Login/Login'
+import NotFound from './pages/Notfound/Notfound'
 
 function AppRoutes () {
   return (
@@ -10,6 +11,7 @@ function AppRoutes () {
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
   )
 }
---
+++ src/pages/Notfound/Notfound.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import { Link } from 'react-router-dom'
+import styles from "./Notfound.module.scss"
+
+const NotFound = () => {
+    return (
+        <div className={styles.NotFoundContainer}>
+            <h1><span>404</span> Page not found</h1>
+            <p>The page you are looking for does not exist</p>
+            <button><Link to={"/"}>Back to Home page</Link></button>
+        </div>
+    )
+}
+
+export default NotFound
