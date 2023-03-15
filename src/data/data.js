d5c9ecd7008bca05be836ca2bbe651cea9987b2f
Jón Ólafur <jolafur4@gmail.com>
2023-03-15 12:14:39 +0000

    Adding Api Events info

--- src/data/data.js
+++ src/data/data.js
@@ -1,56 +1,125 @@
-const imageUrl = 'http://localhost:3000/images'
-
+// Define the data model for an event
 export default [
   {
-    id: 1,
-    title: 'Life Lessons with Katie Zaferes',
-    description: 'I will share with you what I call \'Positively Impactful Moments of Disappointment.\' Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
-    price: 136,
-    coverImg: `${imageUrl}/katie.png`,
-    stats: {
-      rating: 5.0,
-      reviewCount: 6
-    },
-    location: 'Online',
-    openSpots: 0
-  },
-  {
-    id: 2,
-    title: 'Learn Wedding Photography',
-    description: 'Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that\'ll last a lifetime.',
-    price: 125,
-    coverImg: `${imageUrl}/wedding.png`,
-    stats: {
-      rating: 5.0,
-      reviewCount: 30
-    },
-    location: 'Online',
-    openSpots: 27
-  },
-  {
-    id: 3,
-    title: 'Group Mountain Biking',
-    description: 'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
-    price: 50,
-    coverImg: `${imageUrl}/bike.png`,
-    stats: {
-      rating: 4.8,
-      reviewCount: 2
-    },
-    location: 'Norway',
-    openSpots: 3
-  },
-  {
-    id: 4,
-    title: 'Group Mountain Biking',
-    description: 'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
-    price: 50,
-    coverImg: `${imageUrl}/bike.png`,
-    stats: {
-      rating: 4.8,
-      reviewCount: 2
-    },
-    location: 'Norway',
-    openSpots: 3
-  }
-]
+  id: 1,
+  name: 'GoFit',
+  description: 'We are a Fitness Center with all kinds of activities, such as: Zumba, Pilates, Yoga, Spinning, etc',
+  location: 'C. Secretario Padilla, 88, 90, 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 2,
+  name: 'Brooklyn Fitboxing',
+  description: 'Our Fitboxing classes offer you a great cardio workout. You will see your results easily with our app, we will help you to reach your goals.',
+  location: 'C. Fernando Guanarteme, 32, 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 3,
+  name: 'Gran Canaria Spa, Wellness & Health',
+  description: 'We are a spa with all kinds of activities, such as: massages, facials, manicures, pedicures and there is also a jacuzzi and saunas.',
+  location: 'C. Constantino, 3, Planta 1, Oficina 1, 35002 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 4,
+  name: 'Surf Camp Las Palmas',
+  description: 'Come Surfing, we see surfing more as a chance to be connected with the ocean and nature. It is a great way to play sports, discover new places, meet new people and have a good time together. It is a chance to express yourself and be who you are.',
+  location: 'Calle El Salvador, 7, C. Numancia, 19, 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 5,
+  name: 'Bio Finca - Las Tenerías',
+  description: 'We carry our Bio Finca Las Tenerías Gran Canaria with heart and soul. Apart from 6 individually designed residential properties, all of which have a private terrace and are equipped for a comfortable holiday. Our bio farm offers much more! We have been organizing our Fitness & Dance Camp iBiZa since 2014, which has become a major event over the years, with up to 150 participants. The complete program includes everything from Yoga, Kapow Fitness, Zumba Fitness and Zumba in all its variations. We also offer optional excursions, for example the iBiZa Night Tour, catamaran excursions and popular theme parties in the Cala Martina complex, beautifully embedded in a pine forest, directly by the sea..',
+  location: 'Barranco Arteara 7, 35108 Arteara, Las Palmas'
+},
+{
+  id: 6,
+  name: 'Club Voley Playa Net7',
+  description: 'Net 7 was born in Gran Canaria in 2015 with the intention of promoting Beach Volleyball for everyone throughout the year. It is a recreational-sports club, where our main objective is happiness through sports practice in a unique natural space. We are the beach volleyball club with the largest number of licenses nationwide, with more than 250 players.',
+  location: 'Playa de Las Alcaravaneras, 35006, Las Palmas'
+},
+{
+  id: 7,
+  name: 'CrossFit Canarias',
+  description: 'We are a Crossfit station in Las Palmas that offers people special classes and individual workouts.',
+  location: 'Av. República de Nicaragua, 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 8,
+  name: 'Ritiro di Yoga a Fuerteventura',
+  description: 'Yoga Retreat and Energy Reactivation in Las Palmas, Canary Islands (Spain) (accommodation and meals included).',
+  location: 'Av. República de Nicaragua, 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 9,
+  name: 'Breathwork • Free Weekly Class • Las Palmas de Gran Canaria',
+  description: 'Learn the breathing technique. Practice deep breathwork and meditation. Relax, heal and enjoy the benefits. No previous training required.',
+  location: 'Soul Dimension Online Event on Zoom 35001 Las Palmas de Gran Canaria'
+},
+{
+  id: 10,
+  name: 'Pranayama • Free Weekly Class • Las Palmas de Gran Canaria',
+  description: 'Learn the Joy of Breathing technique, practice a deep Pranayama session and enjoy the benefits.',
+  location: 'Soul Dimension Online Event on Zoom 35001 Las Palmas de Gran Canaria'
+},
+{
+  id: 11,
+  name: 'Family Constellations Gran Canaria',
+  description: 'With the aim of healing and freeing yourself from those beliefs, patterns that we frequently repeat, emotions and limiting thoughts. We have therapists and family constellations in Las Palmas de Gran Canaria.',
+  location: 'Gran Canaria Calle Ángel Guimerá, 63 bajo 35004 Gran Canaria'
+},
+{
+  id: 12,
+  name: 'New year Yoga & meditation - Gran Canaria, by the sea, under the stars',
+  description: 'You are warmly invited to join us for New Year Yoga & Meditation with mountain and sea views in the stunning Natural Park of Gui Gui.',
+  location: 'Playa de Tasartico 35478 Gran Canaria'
+},
+{
+  id: 13,
+  name: 'Edentulism2023 - Science. Research. Solutions',
+  description: 'With an incredible panel of speakers, Edentulism2023 will help us make significant improvements in the treatment of edentulous patients.',
+  location: 'ExpoMeloneras - Gran Canaria, Spain Plaza de las Convenciones 35100 Maspalomas'
+},
+{
+  id: 14,
+  name: 'Oceans4Life',
+  description: 'We source alternatives to plastic , and promote local companies who are proving eco products. We promote REDUCE REUSE RECYCLE and collect the bottle tops across the island and donate to a local charity for medical equipment. Our overall mission is to make the islands single use plastic free. Our next challenge is to be able distribute free glass or stainless steel water bottles to every school child on the island . WE are always looking for more help, so please message us with any ideas to help us keep this island beautiful. Beach Cleans and underwater events will be listed here. Our next beach clean will be at Playa El Pajar. Sunday 26th March from 10:00 to 12:00.',
+  location: 'Playa El Pajar. 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 15,
+  name: 'Paleotraining Las Palmas',
+  description: 'PaleoTraining was first conceptualized in 2007 in Lanzarote (Canary Islands), Spain by Airam Fernandez, a physiotherapist and one of the worlds leading experts in Evolutionary Fitness. At his private clinic, he began to notice that an increasing number of his patients suffered from chronic pain, low back pain, neck pain, and muscle injuries. And he also began to realize that he was treating the symptoms but not addressing the causes.',
+  location: 'CC. Daoiz, 25, 35010 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 16,
+  name: 'Dreamfit Las Palmas',
+  description: 'With a top-level professional technical and human team that will attend you personally according to your needs. Our mission is to offer people gyms where they have unique experiences, have fun playing sports and we can help improve your physical condition and health.',
+  location: 'Centro Comercial El Muelle, Muelle de Sta. Catalina, S/N, 35008 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 17,
+  name: 'MacroFit Las Palmas',
+  description: 'Macro Fit Canarias is a 100% Canarian brand, our hashtag #Trainforlife encourages you to prepare well for any obstacle and/or adversity that may occur in each persons daily life. Physical activity is a necessary pillar to lead the healthy and healthy life that we all need. Practicing sports reinforces teamwork, self-improvement, and resistance. It helps us to plan and achieve goals, it gives us self-confidence.',
+  location: 'Av. Juan XXIII, 3, 35004 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 18,
+  name: 'Coworkgym',
+  description: 'Your new training center in the city. Boutique gym, nutritionist, physiotherapist, personal training, sports supplementation, coworkgym (access to external personal trainers), small group classes, and much more..',
+  location: 'C. Luis Doreste Silva, 107, 35004 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 19,
+  name: 'Cenbox',
+  description: 'Through our Crosstraining service we want to help you enjoy the best possible quality of life. Our varied daily classes suitable for all levels will make you spend an unrepeatable time. The fundamental goal is for our clients to achieve their goals in a healthy way and enjoying each workout..',
+  location: 'C. Pío XII, 64, 35006 Las Palmas de Gran Canaria, Las Palmas'
+},
+{
+  id: 20,
+  name: 'Canaryecoevents',
+  description: 'Living life in a more conscious and calm way is possible. Improving our health is the responsibility of each one of us. For this, it is necessary to calm the mind by paying attention to the body, eating healthy and integrating natural and sustainable spaces into our daily lives. We are clear that the personal dream is necessary to contribute value to society. We also know that it is easier to go further if you are accompanied by people who have the same goal as you. Canaryecoevents aims to be an example of an initiative that promotes synergies with other professionals. With the purpose of offering tools to manage stress, promote healthy habits, improve nutrition, raise awareness of the importance of caring for the environment and cultivate solidarity. Creating projects to sensitize companies and institutions to increase investment in this type of activity.',
+  location: 'Cl. Doña Pino Apolinario, 48, 35014 Las Palmas de Gran Canaria, Las Palmas'
+},
+];
+
+
